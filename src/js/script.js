5f466302996bd49230ee5666eca1316cbecee917
Beatriz <beatrizdossantos6666@gmail.com>
2026-03-27 09:50:35 -0300

    feat: Declarações de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// DECLARAÇÃO E VARIAVEIS
+// VAR, LET e CONST
+
+var nome="Neo";
+console.log(nome);
+var nome="Dream";
+console.log(nome);
+
+if(true){
+    let avatar="Fiapinho";
+}
+
+console.log(avatar);
